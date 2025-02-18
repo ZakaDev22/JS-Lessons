88a121ac5205395d793a80b7e3a7d19177115a43
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-18 23:54:03 +0000

    Add Converting Numbers Practice

---
+++ ConvertingTypesToNumbers.js
@@ -0,0 +1,18 @@
+
+
+console.log('32' - '23'); // 9
+
+console.log(+'23'); // 23
+console.log(+'zz'); // nan
+console.log(+true); // 1
+console.log(+false);// 0
+console.log(+undefined); // nan
+console.log(+null); // 0
+
+// Another Ways
+
+console.log(Number('4'));
+
+console.log(parseInt('4'));
+
+console.log(parseFloat('23.1'));
