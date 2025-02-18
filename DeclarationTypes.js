fca08fef1263cc27a7752d8833076f7b3231bff8
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-18 22:24:19 +0000

    Declarations Types

---
+++ DeclarationTypes.js
@@ -0,0 +1,18 @@
+
+var a = 'a';
+a = 'koko';
+
+console.log(a);
+
+a = '23';
+console.log(a);
+
+let x = 'k';
+console.log(x);
+x = 23 ;
+console.log(x);
+
+const f = 'fff';
+console.log(f);
+// f = 6;
+// console.log(f);
