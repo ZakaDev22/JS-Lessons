8c8ce14249224e5c120c42e30ec633a7c9bda8d2
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-19 00:09:10 +0000

    Number Object Practice

---
+++ NumberObject.js
@@ -0,0 +1,15 @@
+
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+// validation
+
+console.log(Number.isSafeInteger(23)); // true
+
+console.log(Number.isInteger(-23)); // true
+
+console.log(Number.isNaN(23 / 'zaka')); // ture
