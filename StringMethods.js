0a59af8a290ac772801a32ef824081197e72100c
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-19 16:34:34 +0000

    String Methods Practice

---
+++ StringMethods.js
@@ -0,0 +1,33 @@
+
+
+let MyName = 'Zakaria';
+
+console.log(MyName.length);
+
+console.log(MyName.indexOf('a'));
+
+console.log(MyName.lastIndexOf('a'));
+
+console.log(MyName.charAt(2));
+
+console.log(MyName[0]);
+
+console.log(MyName.charCodeAt(0));
+
+console.log(MyName.substring(2, 5));
+
+console.log(MyName.includes('Zakaria'));
+
+console.log(MyName.toUpperCase());
+
+console.log(MyName.toLowerCase());
+
+let hhh = '  lorem ipsum dolor sit amet consectetur adipisicing elit.  ';	
+
+console.log(hhh.split(' '));
+
+console.log(hhh.replace('lorem', 'Zakaria'));
+
+console.log(hhh.slice(0, 5));
+
+console.log(hhh.trim());
