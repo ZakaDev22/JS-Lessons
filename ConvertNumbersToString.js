b5bc8712ff5364830e88931e74d54efce6e050c7
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-19 16:03:23 +0000

    Four Ways To Convert Numbers To String

---
+++ ConvertNumbersToString.js
@@ -0,0 +1,12 @@
+
+let z = 23;
+
+console.log( typeof z);
+
+console.log( typeof String(z));
+
+console.log( typeof z.toString());
+
+console.log( typeof 23..toString());
+
+console.log( typeof (23).toString());
