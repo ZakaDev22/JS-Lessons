7da97a31eb842bd0be9634082c110fe487a2c7b0
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-20 23:12:50 +0000

    Add concat and join methods examples to ArraysP3.js

--- ArraysP3.js
+++ ArraysP3.js
@@ -12,3 +12,14 @@ console.log("Sorted in descending order:", numbers); // Output: [8, 5, 3, 2, 1]
 // Reversing the array
 numbers.reverse();
 console.log("Reversed array:", numbers); // Output: [1, 2, 3, 5, 8]
+
+// concat and jion methods
+
+let Names = ['Zakaria','meliodas','John', 'Jane', 'Mary'];
+let Names2 = ['Mark', 'Paul'];
+
+console.log(Names.concat(Names2)); // Output: ['Zakaria','meliodas','John', 'Jane', 'Mary', 'Mark', 'Paul']
+
+console.log(Names.concat(Names2,'king','queen')); // Output: ['Zakaria','meliodas','John', 'Jane', 'Mary', 'Mark', 'Paul','king','queen']
+
+console.log(Names.join(', ')); // Output: 'Zakaria, meliodas, John, Jane, Mary'
