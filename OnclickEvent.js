f1df7353dc293c845e67dfcb119502365b593ea6
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-03-02 15:51:49 +0000

    Add OnclickEvent.js for button click handling; update index.html to include the new script and comment out CreateHTMLPage.js

---
+++ OnclickEvent.js
@@ -0,0 +1,9 @@
+// Example using onclick
+document.getElementById("myButton").onclick = function() {
+    alert("Button clicked!");
+};
+
+// Example using addEventListener
+document.getElementById("myButton").addEventListener("click", () => {
+    alert("Button clicked!");
+});
