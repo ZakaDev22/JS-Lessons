67e05bef46e232bfdbafa7fe11e1b19d800989b9
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-03-03 15:09:50 +0000

    Add AddRemoveToggleClass.js for class manipulation on DOM elements

---
+++ AddRemoveToggleClass.js
@@ -0,0 +1,11 @@
+// Select the element
+const element = document.querySelector('#myElement');
+
+// Add a class
+element.classList.add('newClass');
+
+// Remove a class
+element.classList.remove('oldClass');
+
+// Toggle a class
+element.classList.toggle('activeClass');
