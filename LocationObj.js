b201ad04e85ded33203ac7171adb35c66b492522
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-03-05 00:53:04 +0000

    Add LocationObj.js for demonstrating location object properties and methods

---
+++ LocationObj.js
@@ -0,0 +1,49 @@
+// The location object contains information about the current URL.
+
+// Example of location.href
+// Returns the entire URL of the current page
+console.log("location.href: " + location.href);
+
+// Example of location.protocol
+// Returns the protocol of the current URL (http: or https:)
+console.log("location.protocol: " + location.protocol);
+
+// Example of location.host
+// Returns the hostname and port number of the current URL
+console.log("location.host: " + location.host);
+
+// Example of location.hostname
+// Returns the hostname of the current URL
+console.log("location.hostname: " + location.hostname);
+
+// Example of location.port
+// Returns the port number of the current URL
+console.log("location.port: " + location.port);
+
+// Example of location.pathname
+// Returns the path and filename of the current URL
+console.log("location.pathname: " + location.pathname);
+
+// Example of location.search
+// Returns the query string part of the URL (including the ?)
+console.log("location.search: " + location.search);
+
+// Example of location.hash
+// Returns the anchor part of the URL (including the #)
+console.log("location.hash: " + location.hash);
+
+// Example of location.origin
+// Returns the protocol, hostname, and port number of the URL
+console.log("location.origin: " + location.origin);
+
+// Example of location.assign()
+// Loads a new document
+// location.assign('https://www.example.com');
+
+// Example of location.reload()
+// Reloads the current document
+// location.reload();
+
+// Example of location.replace()
+// Replaces the current document with a new one
+// location.replace('https://www.example.com');
