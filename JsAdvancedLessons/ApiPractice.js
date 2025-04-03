81c997b05a8557c6ee09ff568ae49a4610942883
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-04-03 13:49:45 +0000

    Refactor OldHTTPRequest.html to use external ApiPractice.js for XMLHttpRequest examples

---
+++ JsAdvancedLessons/ApiPractice.js
@@ -0,0 +1,32 @@
+let request = new XMLHttpRequest();
+request.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+request.send();
+request.onload = function () {
+    if (this.status >= 200 && this.status < 300) {
+        let data = JSON.parse(this.responseText);
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', this.status);
+    }
+};
+request.onerror = function () {
+    console.error('Request failed');
+};
+
+let request2 = new XMLHttpRequest();
+request2.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+request2.responseType = 'json'; // Set the response type to JSON
+request2.send();
+request2.onload = function () {
+    if (this.status >= 200 && this.status < 300) {
+        let data = this.response; // No need to parse, it's already JSON
+        data.forEach(post => {
+            console.log(`Title: ${post.title}`);
+        });
+    } else {
+        console.error('Request failed with status:', this.status);
+    }
+};
+request2.onerror = function () {
+    console.error('Request failed');
+};
