c04fc493f75dd850a5aedc149e1f16bb61879c3b
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-04-09 10:50:51 +0100

    Refactor user list rendering and add function to fetch users in MiniApiProjectWithFakeData

--- JsAdvancedLessons/MiniApiProjectWithFakeData/apiProject.js
+++ JsAdvancedLessons/MiniApiProjectWithFakeData/apiProject.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
  const Datacontainer = document.getElementById('data-container');
+ const ListContainer = document.querySelector('#users-list ul');
 
  function GetAllPostsByUserID(userID) {
     let request2 = new XMLHttpRequest();
@@ -12,9 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (this.status >= 200 && this.status < 300) {
             let data = this.response; // No need to parse, it's already JSON
             data.forEach(post => {
-                let postElement = document.createElement('div');
-                postElement.innerText = `UserID: ${post.userId}, Title: ${post.title}`;
-                container.appendChild(postElement);
+                console.log(post);
             });
         } else {
             console.error('Request failed with status:', this.status);
@@ -24,6 +23,29 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Request failed');
     };
 }
+
+ function GetAllUserUsers() {
+    let request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://jsonplaceholder.typicode.com/users`, true);
+    request2.responseType = 'json'; // Set the response type to JSON
+    request2.send();
+    request2.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+            let data = this.response; // No need to parse, it's already JSON
+            data.forEach(post => {
+                let postElement = document.createElement('li');
+               
+            });
+        } else {
+            console.error('Request failed with status:', this.status);
+        }
+    };
+    request2.onerror = function () {
+        console.error('Request failed');
+    };
+}
+
 
 
 });
